3a25009dd69c396ec1b20168d9900eb9696c2316
monsteronfire <monsteronfire@gmail.com>
2016-06-25 19:10:50 +0800

    Include es2015

--- src/Person.js
+++ src/Person.js
@@ -1,1 +1,11 @@
-class Person {}
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  greet() {
+    return this.name + ' says hello from the outside.';
+  }
+}
+
+console.log(new Person('Shaii').greet());
