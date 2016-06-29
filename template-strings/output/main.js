ddf9292591b5154faf55e56ee3ebb702baef48de
monsteronfire <monsteronfire@gmail.com>
2016-06-29 08:30:18 +0800

    Add template strings example

---
+++ template-strings/output/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var name = "Adele";
+
+var template = "\n  <div class=\"Alert\">\n    <p>" + name + "</p>\n  </div>\n";
+
+console.log(template);
---
+++ template-strings/src/main.js
@@ -0,0 +1,9 @@
+let name = 'Adele';
+
+let template = `
+  <div class="Alert">
+    <p>${name}</p>
+  </div>
+` ;
+
+console.log(template);
