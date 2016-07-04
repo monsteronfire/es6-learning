856b4b1e6263493ac1ccefd40ff6d069fabe4b6c
monsteronfire <monsteronfire@gmail.com>
2016-07-04 08:56:38 +0800

    Add example for module bundling with webpack

---
+++ module-bundling-with-webpack/src/main.js
@@ -0,0 +1,8 @@
+import TaskCollection from './TaskCollection';
+
+new TaskCollection([
+  'Take shorty to the movies',
+  'Make shorty my baby',
+  'Get the popcorn',
+  'Know what else she likes'
+]).dump();
